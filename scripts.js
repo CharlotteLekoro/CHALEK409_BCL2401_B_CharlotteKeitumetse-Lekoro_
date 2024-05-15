5c26e1c2dc2104a7bdb575d908789be403254704
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-05-15 14:56:42 +0200

    More event listeners added
    I have added more event listeners under the function i created, i am doing to make the code more readable and abstract

--- scripts.js
+++ scripts.js
@@ -64,6 +64,34 @@ function setupEventListeners() {
         document.querySelector("[data-settings-overlay]").open = false;
     });
 
+    document.querySelector("[data-header-search]").addEventListener("click", () => {
+        document.querySelector("[data-search-overlay]").open = true;
+        document.querySelector("[data-search-title]").focus();
+    });
+
+    document.querySelector("[data-header-settings]").addEventListener("click", () => {
+        document.querySelector("[data-settings-overlay]").open = true;
+    });
+
+    document.querySelector("[data-list-close]").addEventListener("click", () => {
+        document.querySelector("[data-list-active]").open = false;
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 document.querySelector("[data-search-authors]").appendChild(authorsHtml);
 
 if (
