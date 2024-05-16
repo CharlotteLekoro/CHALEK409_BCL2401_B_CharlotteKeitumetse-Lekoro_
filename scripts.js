103974db191e75677c810f931b420fcb8ff63229
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-05-16 10:06:43 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -56,64 +56,120 @@ for (const [id, name] of Object.entries(authors)) {
 }
 
 function setupEventListeners() {
-    document.querySelector("[data-search-cancel]").addEventListener("click", () => {
-        document.querySelector("[data-search-overlay]").open = false;
+  document
+    .querySelector("[data-search-cancel]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-search-overlay]").open = false;
     });
 
-    document.querySelector("[data-settings-cancel]").addEventListener("click", () => {
-        document.querySelector("[data-settings-overlay]").open = false;
+  document
+    .querySelector("[data-settings-cancel]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-settings-overlay]").open = false;
     });
 
-    document.querySelector("[data-header-search]").addEventListener("click", () => {
-        document.querySelector("[data-search-overlay]").open = true;
-        document.querySelector("[data-search-title]").focus();
+  document
+    .querySelector("[data-header-search]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-search-overlay]").open = true;
+      document.querySelector("[data-search-title]").focus();
     });
 
-    document.querySelector("[data-header-settings]").addEventListener("click", () => {
-        document.querySelector("[data-settings-overlay]").open = true;
+  document
+    .querySelector("[data-header-settings]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-settings-overlay]").open = true;
     });
 
-    document.querySelector("[data-list-close]").addEventListener("click", () => {
-        document.querySelector("[data-list-active]").open = false;
+  document.querySelector("[data-list-close]").addEventListener("click", () => {
+    document.querySelector("[data-list-active]").open = false;
+  });
+
+  document
+    .querySelector("[data-settings-form]")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const { theme } = Object.fromEntries(formData);
+      applyTheme(theme);
+      document.querySelector("[data-settings-overlay]").open = false;
     });
 
-    document.querySelector("[data-settings-form]").addEventListener("submit", (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const { theme } = Object.fromEntries(formData);
-        applyTheme(theme);
-        document.querySelector("[data-settings-overlay]").open = false;
+  document
+    .querySelector("[data-search-form]")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const filters = Object.fromEntries(formData);
+      const result = filterBooks(filters);
+      page = 1;
+      matches = result;
+      document.querySelector("[data-list-items]").innerHTML = "";
+      updateBookList(result);
+      updateShowMoreButton(result);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      document.querySelector("[data-search-overlay]").open = false;
     });
 
-    
-    document.querySelector("[data-search-form]").addEventListener("submit", (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const filters = Object.fromEntries(formData);
-        const result = filterBooks(filters);
-        page = 1;
-        matches = result;
-        document.querySelector("[data-list-items]").innerHTML = "";
-        updateBookList(result);
-        updateShowMoreButton(result);
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        document.querySelector("[data-search-overlay]").open = false;
+  document.querySelector("[data-list-button]").addEventListener("click", () => {
+    const fragment = document.createDocumentFragment();
+    for (const { author, id, image, title } of matches.slice(
+      page * BOOKS_PER_PAGE,
+      (page + 1) * BOOKS_PER_PAGE
+    )) {
+      const element = document.createElement("button");
+      element.classList = "preview";
+      element.setAttribute("data-preview", id);
+      element.innerHTML = `
+                <img class="preview__image" src="${image}" />
+                <div class="preview__info">
+                    <h3 class="preview__title">${title}</h3>
+                    <div class="preview__author">${authors[author]}</div>
+                </div>`;
+      fragment.appendChild(element);
+    }
+    document.querySelector("[data-list-items]").appendChild(fragment);
+    page += 1;
+    updateShowMoreButton(matches);
+  });
+
+  document
+    .querySelector("[data-list-items]")
+    .addEventListener("click", (event) => {
+      const pathArray = Array.from(event.path || event.composedPath());
+      let active = null;
+      for (const node of pathArray) {
+        if (node?.dataset?.preview) {
+          active = books.find((book) => book.id === node.dataset.preview);
+          if (active) break;
+        }
+      }
+      if (active) {
+        document.querySelector("[data-list-active]").open = true;
+        document.querySelector("[data-list-blur]").src = active.image;
+        document.querySelector("[data-list-image]").src = active.image;
+        document.querySelector("[data-list-title]").innerText = active.title;
+        document.querySelector("[data-list-subtitle]").innerText = `${
+          authors[active.author]
+        } (${new Date(active.published).getFullYear()})`;
+        document.querySelector("[data-list-description]").innerText =
+          active.description;
+      }
     });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function filterBooks(filters) {
+  return books.filter((book) => {
+    const genreMatch =
+      filters.genre === "any" || book.genres.includes(filters.genre);
+    const titleMatch =
+      !filters.title.trim() ||
+      book.title.toLowerCase().includes(filters.title.toLowerCase());
+    const authorMatch =
+      filters.author === "any" || book.author === filters.author;
+    return genreMatch && titleMatch && authorMatch;
+  });
+}
 
 document.querySelector("[data-search-authors]").appendChild(authorsHtml);
 
